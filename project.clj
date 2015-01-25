59c96410b754a08fb75ed71a8f88536109f20dc8
Christopher Small <metasoarous@gmail.com>
2015-01-25 11:45:06 -0800

    Don't need trace here really

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.trace "0.7.8"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [prismatic/plumbing "0.3.5"]])
 
