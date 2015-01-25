b5e89e6c4bec3b622e98458209dc3e5845e9a4c8
Christopher Small <metasoarous@gmail.com>
2015-01-24 19:16:26 -0800

    Started integrating marginalia

--- doc/intro.md
+++
@@ -1,4 +0,0 @@
-# Introduction to semantic-csv
-
-Coming soon!
-
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject semantic-csv "0.1.0-SNAPSHOT"
   :description "A Clojure library with higher level CSV parsing functionality"
   :url "http://github.com/metasoarous/semantic-csv"
+  :plugins [[lein-marginalia "0.8.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
