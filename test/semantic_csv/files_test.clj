85944143ccec3b5b61b007625cf558b1f85cb6f7
Christopher Small <metasoarous@gmail.com>
2015-02-23 16:58:17 -0800

    Added spit test

--- test/semantic_csv/files_test.clj
+++ test/semantic_csv/files_test.clj
@@ -1,9 +1,12 @@
 (ns semantic-csv.files-test
   (:require [clojure.test :refer :all]
+            [clojure-csv.core :as csv]
+            [clojure.java.io :as io]
             [semantic-csv.core :refer :all]))
 
 
 (def test-fn "test/test.csv")
+
 
 (deftest slurp-csv-test
   (testing "should work"
@@ -12,5 +15,17 @@
   (testing "should work with cast-fns"
     (is (= (first (slurp-csv test-fn :cast-fns {:this ->int}))
            {:this 1 :that "2" :more "stuff"}))))
+
+
+(deftest spit-csv-test
+  (let [data [["this" "that"]
+              ["1" "2"]
+              ["3" "4"]]
+        test-f "test/testout.csv"]
+    (testing "basic spitting"
+      (is (= (do (spit-csv test-f data)
+                 (with-open [f (io/reader test-f)]
+                   (doall (csv/parse-csv f))))
+             data)))))
 
 
