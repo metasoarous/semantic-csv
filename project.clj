4b2447175d3de501155254038133559f56de14cf
Christopher T. Small <metasoarous@gmail.com>
2017-03-06 16:28:29 +0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject semantic-csv "0.2.0"
+(defproject semantic-csv "0.2.1-alpha1"
   :description "A Clojure library with higher level CSV parsing functionality"
   :url "http://github.com/metasoarous/semantic-csv"
   :license {:name "Eclipse Public License"
