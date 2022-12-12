f55cafb4d1c3fc0af4aa6cb849962a6090c19e79
Michael Easter <measter@splashfinancial.com>
2022-12-12 10:16:38 -0600

    Remove clojurescript dependency

--- project.clj
+++ project.clj
@@ -4,11 +4,8 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.9.493"]
                  [clojure-csv/clojure-csv "2.0.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                                   [com.cemerick/piggieback "0.2.1"]]
                    :plugins      [[lein-marginalia "0.9.0"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
-
-
